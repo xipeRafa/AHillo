8131abb8d60ca7db1e6840099c1cd5bfc7631021
fer <fer@192.168.1.6>
2024-06-24 04:16:21 -0700

    center

--- src/components/authentication/NavComp.js
+++ src/components/authentication/NavComp.js
@@ -8,13 +8,16 @@ export const NavComp = () => {
   const { currentUser, logout } = useContext(AuthContext);
 
   return (
-    <nav className="container navbar navbar-light ">
-      <div className="container d-flex flex-column">
+    <nav className="container navbar navbar-light">
+
+      <div className="w-100 d-flex justify-content-center">
         <div className="navbar-brand mb-4">
-          <img src={logoImg} alt="logo" height="160" />
+          <img src={logoImg} alt="logo" height="140" />
         </div>
-        <div className="d-flex">
-          <div className="col mb-4">
+      </div>
+
+      <div className="w-100 d-flex justify-content-center">
+          <div className="mb-4">
             {currentUser ? (
               <>
                 <div className="btn btn-secondary mx-2 disabled">
@@ -31,8 +34,8 @@ export const NavComp = () => {
               </>
             )}
           </div>
-        </div>
-      </div>
+      </div>  
+
     </nav>
   );
 };
